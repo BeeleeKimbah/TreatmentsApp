d3c684e338bbc7bb4a627114d289d388c446cf53
BeeleeKimbah <92517800+BeeleeKimbah@users.noreply.github.com>
2024-05-09 14:56:45 +0300

    Implemented bottom navigation bar in line with Compose Destinations library features

--- app/src/main/java/com/razvanberchez/proiectlicenta/view/components/BottomBar.kt
+++ app/src/main/java/com/razvanberchez/proiectlicenta/view/components/BottomBar.kt
@@ -1,11 +1,10 @@
 package com.razvanberchez.proiectlicenta.view.components
 
+import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.outlined.ExitToApp
 import androidx.compose.material.icons.outlined.List
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.Settings
@@ -14,75 +13,75 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
-import androidx.navigation.compose.currentBackStackEntryAsState
 import com.ramcosta.composedestinations.navigation.navigate
+import com.ramcosta.composedestinations.spec.DirectionDestinationSpec
 import com.razvanberchez.proiectlicenta.R
-import com.razvanberchez.proiectlicenta.data.model.NavigationItem
-import com.razvanberchez.proiectlicenta.view.screen.destinations.LoginScreenDestination
+import com.razvanberchez.proiectlicenta.view.screen.NavGraphs
+import com.razvanberchez.proiectlicenta.view.screen.appCurrentDestinationAsState
+import com.razvanberchez.proiectlicenta.view.screen.destinations.Destination
 import com.razvanberchez.proiectlicenta.view.screen.destinations.MedicsScreenDestination
 import com.razvanberchez.proiectlicenta.view.screen.destinations.SessionsScreenDestination
+import com.razvanberchez.proiectlicenta.view.screen.startAppDestination
 
 val bottomNavScreenRoutes = listOf(
     MedicsScreenDestination.route,
     SessionsScreenDestination.route
 )
 
+enum class BottomBarDestinations (
+    val direction: DirectionDestinationSpec,
+    @StringRes val title: Int,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+) {
+    Sessions(
+        SessionsScreenDestination,
+        R.string.menu_item_MySessions,
+        Icons.Filled.List,
+        Icons.Outlined.List
+    ),
+    Medics(
+        MedicsScreenDestination,
+        R.string.menu_item_Medics,
+        Icons.Filled.Person,
+        Icons.Outlined.Person
+    ),
+    Settings(
+        MedicsScreenDestination,
+        R.string.menu_item_Settings,
+        Icons.Filled.Settings,
+        Icons.Outlined.Settings
+    )
+}
+
 @Composable
 fun BottomBar(navController: NavController) {
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentDestination = navBackStackEntry?.destination
+    val currentDestination: Destination = navController.appCurrentDestinationAsState().value
+        ?: NavGraphs.root.startAppDestination
 
-    if (currentDestination?.route in bottomNavScreenRoutes) {
+    if (currentDestination.route in bottomNavScreenRoutes) {
         NavigationBar {
-            val navItems = listOf(
-                NavigationItem(
-                    title = stringResource(R.string.menu_item_MySessions),
-                    selectedIcon = Icons.Filled.List,
-                    unselectedIcon = Icons.Outlined.List,
-                    destination = SessionsScreenDestination
-                ),
-                NavigationItem(
-                    title = stringResource(R.string.menu_item_Medics),
-                    selectedIcon = Icons.Filled.Person,
-                    unselectedIcon = Icons.Outlined.Person,
-                    destination = MedicsScreenDestination
-                ),
-                NavigationItem(
-                    title = stringResource(R.string.menu_item_Settings),
-                    selectedIcon = Icons.Filled.Settings,
-                    unselectedIcon = Icons.Outlined.Settings,
-                    destination = MedicsScreenDestination
-                ),
-                NavigationItem(
-                    title = stringResource(R.string.menu_item_Logout),
-                    selectedIcon = Icons.Filled.ExitToApp,
-                    unselectedIcon = Icons.Outlined.ExitToApp,
-                    destination = LoginScreenDestination
-                )
-            )
-            var selectedIndex by rememberSaveable {
-                mutableStateOf(0)
-            }
-
-            navItems.forEachIndexed { index, item ->
+            BottomBarDestinations.entries.forEach { item ->
                 NavigationBarItem(
-                    selected = selectedIndex == index,
-                    label = { Text(item.title) },
+                    selected = currentDestination == item.direction,
+                    label = { Text(stringResource(item.title)) },
                     onClick = {
-                        selectedIndex = index
-                        /* TODO: Navigate */
-                        navController.navigate(item.destination)
+                        navController.navigate(item.direction) {
+                            launchSingleTop = true
+                            popUpTo(item.direction.route) {
+                                inclusive = true
+                            }
+                        }
                     },
                     icon = {
                         Icon(
-                            imageVector = if (index == selectedIndex) item.selectedIcon
-                            else item.unselectedIcon,
+                            imageVector = if (currentDestination == item.direction)
+                                            item.selectedIcon
+                                        else
+                                            item.unselectedIcon,
                             contentDescription = null
                         )
                     }
