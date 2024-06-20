ffc0c1deee8a0dce3d6bf2adcc11c6be45f53bc9
BeeleeKimbah <92517800+BeeleeKimbah@users.noreply.github.com>
2024-06-20 19:57:37 +0300

    Fixed pre-selected treatment start date

--- app/src/main/java/com/razvanberchez/proiectlicenta/view/viewstate/AddTreatmentScreenViewState.kt
+++ app/src/main/java/com/razvanberchez/proiectlicenta/view/viewstate/AddTreatmentScreenViewState.kt
@@ -1,6 +1,8 @@
 package com.razvanberchez.proiectlicenta.view.viewstate
 
 import com.razvanberchez.proiectlicenta.data.model.TimeSlot
+import com.razvanberchez.proiectlicenta.presentation.getDateWithoutTime
+import com.razvanberchez.proiectlicenta.presentation.getNextDay
 import java.util.Date
 
 data class AddTreatmentScreenViewState(
@@ -8,7 +10,7 @@ data class AddTreatmentScreenViewState(
     val dose: Int? = null,
     val frequency: Int? = null,
     val applications: Int? = null,
-    val startDate: Date = Date(),
+    val startDate: Date = Date().getNextDay().getDateWithoutTime(),
     val addButtonEnabled: Boolean = false,
     val startTime: TimeSlot = TimeSlot(8, 0),
     val treatmentAdded: Boolean = false
